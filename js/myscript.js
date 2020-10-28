4c173a95b016adc5aa3c2caff68ac8f20636487e
Anna <rosalenanna@gmail.com>
2020-10-28 15:07:46 +0100

    struttura logica

---
+++ js/myscript.js
@@ -0,0 +1,26 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+
+
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+
+// I numeri non possono essere duplicati (i 16 numeri devono essere diversi tra loro)
+
+// chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
+// numeri utente devono essere diversi tra loro
+
+// CICLO
+  // se numero utente è uguale a numeri computer: game over
+    // outuput: game over + numero di volte che utente ha inserito numero diverso
+
+  // se utente raggiunge limite di 84 numeri : game over
+
+  // se numero utente diverso da numeri computer : utente inserisce un altro numero
