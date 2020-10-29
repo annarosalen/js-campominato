a6a1c9bcabe6aa3cb5fb3b984c97545047f34f3c
Anna <rosalenanna@gmail.com>
2020-10-29 12:04:49 +0100

    ciclo

--- js/myscript.js
+++ js/myscript.js
@@ -9,18 +9,67 @@
 
 
 
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// Il computer deve generare 16 numeri casuali tra 1 e 100: creo ciclo di 16 ripetizioni
 
-// I numeri non possono essere duplicati (i 16 numeri devono essere diversi tra loro)
+var arrayNumPc = [];
 
-// chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-
-// numeri utente devono essere diversi tra loro
+for (var i = 0; arrayNumPc.length < 16; i++) {
+  var numPc = arrayNumPc[i];
+  numPc = getRandomIntInclusive(1,100);
+  // I numeri non possono essere duplicati (i 16 numeri devono essere diversi tra loro)
+  if(arrayNumPc.indexOf(numPc) === -1) {
+    arrayNumPc.push(numPc);
+  }
+}
+console.log(arrayNumPc);
 
 // CICLO
-  // se numero utente è uguale a numeri computer: game over
-    // outuput: game over + numero di volte che utente ha inserito numero diverso
 
-  // se utente raggiunge limite di 84 numeri : game over
+var arrayNumUtente = [];
+
+var risultato = "win";
+
+// chiedere all’utente (100 - 16) (3 prova) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+  var i = 0;
+  while (i < 3) {
+
+    var numUtente = arrayNumUtente[i];
+    numUtente = parseInt(prompt("inserisci un numero tra 1 e 100"));
+    // numeri utente devono essere diversi tra loro
+    if(arrayNumUtente.indexOf(numUtente) === -1){
+      arrayNumUtente.push(numUtente);
+    }
+    // se numero utente è uguale a numeri computer: game over
+    if (numUtente == numPc){
+      // outuput: game over + numero di volte che utente ha inserito numero diverso da pc
+      risultato = "gameover";
+    }
+
+    i++;
+  }
+  console.log(arrayNumUtente);
+
+
+if (risultato == "gameover"){
+  console.log("game over", arrayNumUtente.length)
+}
+
+
+
+// CICLO
+// se numero utente è uguale a numeri computer: game over
+  // outuput: game over + numero di volte che utente ha inserito numero diverso
+
+  // se utente raggiunge limite di 84 numeri : hai vinto
 
   // se numero utente diverso da numeri computer : utente inserisce un altro numero
+
+
+
+// FUNZIONI UTILI
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Il max è incluso e il min è incluso
+  }
