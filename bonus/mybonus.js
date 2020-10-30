40c15a8b3b78c1a202bf4550fb520a6104a6ccf7
Anna <rosalenanna@gmail.com>
2020-10-30 15:51:33 +0100

    sostituito break

---
+++ bonus/mybonus.js
@@ -0,0 +1,6 @@
+// // difficoltà uno ( da 1 a 80)
+// console.log(functionRandom(1,80));
+// console.log(functionUtente(1,80));
+// // difficoltà due ( da 1 a 50)
+// console.log(functionRandom(1,50));
+// console.log(functionUtente(1,50));
--- js/myscript.js
+++ js/myscript.js
@@ -13,50 +13,66 @@
 
 var arrayNumPc = [];
 
-for (var i = 0; arrayNumPc.length < 16; i++) {
-  var numPc = arrayNumPc[i];
-  numPc = getRandomIntInclusive(1,100);
-  // I numeri non possono essere duplicati (i 16 numeri devono essere diversi tra loro)
-  if(arrayNumPc.indexOf(numPc) === -1) {
-    arrayNumPc.push(numPc);
+function functionRandom(numMin, numMax) {
+  for (var i = 0; arrayNumPc.length < 16; i++) {
+    var numPc = arrayNumPc[i];
+    numPc = getRandomIntInclusive(numMin, numMax);
+    // I numeri non possono essere duplicati (i 16 numeri devono essere diversi tra loro)
+    if(arrayNumPc.indexOf(numPc) === -1) {
+      arrayNumPc.push(numPc);
+    }
   }
+  return arrayNumPc;
 }
-console.log(arrayNumPc);
+
+
+
+// BONUS************************* (gli altri livelli su mybonus.js)
+
+// difficoltà zero range di numPc( da 1 a 100)
+console.log(functionRandom(1,100));
+
+
 
 // CICLO
 
 var arrayNumUtente = [];
 
+function functionUtente(num1, num2){
 
-// chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-var i = 0;
-while (i < 3) {
+  // chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+  var i = 0;
+  while (i < 4) {
 
-  var numUtente = arrayNumUtente[i];
-  numUtente = parseInt(prompt("inserisci un numero tra 1 e 100"));
-  // numeri utente devono essere diversi tra loro
-  if(arrayNumUtente.includes(numUtente) || (numUtente < 1) || (numUtente > 100)){
-    alert("ERRORE! Il numero che hai inserito non è corretto");
-    // se numero utente è all'interno di array computer: game over
-  }else if(arrayNumPc.includes(numUtente)){
+    var numUtente = arrayNumUtente[i];
+    numUtente = parseInt(prompt("inserisci un numero tra 1 e 100 NB: non puoi ripetere un numero già scelto in precendenza"));
+    // numeri utente devono essere diversi tra loro
+    if(arrayNumUtente.includes(numUtente)|| (numUtente < num1) || (numUtente > num2)){
+      alert("ERRORE! Il numero che hai inserito non è corretto");
+      i--;
+      // se numero utente è all'interno di array computer: game over
+    }else if(arrayNumPc.includes(numUtente)){
       // outuput: game over + numero di volte che utente ha inserito numero diverso da pc
-    console.log("game over", arrayNumUtente.length);
-  break;
-    // se numero utente diverso da numeri computer : utente inserisce un altro numero
-  }else{
-    arrayNumUtente.push(numUtente);
+      return "game over" + " " + arrayNumUtente.length
+
+      // se numero utente diverso da numeri computer : utente inserisce un altro numero
+    }else{
+      arrayNumUtente.push(numUtente);
+    }
+
+    i++;
+
   }
 
-  i++;
-}
-
-// se utente raggiunge limite di 84 numeri : hai vinto
-
-if  (arrayNumUtente.length === 3){
-  console.log("hai vinto",arrayNumUtente.length);
-}
+  // se utente raggiunge limite di 84 numeri : hai vinto
+  if  (arrayNumUtente.length === 4){
+    console.log("hai vinto",arrayNumUtente.length);
+  }
 
 
+} //fine functionUtente
+
+console.log(functionUtente(1,100))
 
 
 
